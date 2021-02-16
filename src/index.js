cfd3d4f1e5df7af934b389d8a3767053a340e2dd
Kartik Ahuja <kartik.ahuja24@gmail.com>
2021-02-17 02:06:19 +0530

    adding new slice projects

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import configureStore from "./store/configureStore";
 import { bugAdded, bugResolved } from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
 
-store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugResolved({ id: 1 }));
+
+store.dispatch(projectAdded({ name: "Project 1" }));
 
 console.log(store.getState());
