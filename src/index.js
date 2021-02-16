6f9495d0e0db1d3b3af6c1533a455df9993b6b9c
Kartik Ahuja <kartik.ahuja24@gmail.com>
2021-02-17 01:22:55 +0530

    Refactoring Action creators with redux toolkit createAction fn

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { bugAdded, bugResolved } from "./store/bugs";
 
 const store = configureStore();
 
-store.dispatch(bugAdded("Bug 1"));
-store.dispatch(bugAdded("Bug 2"));
-store.dispatch(bugAdded("Bug 3"));
+store.dispatch(bugAdded({ description: "Bug 1" }));
+store.dispatch(bugAdded({ description: "Bug 2" }));
+store.dispatch(bugAdded({ description: "Bug 3" }));
 
-store.dispatch(bugResolved(1));
+store.dispatch(bugResolved({ id: 1 }));
 
 console.log(store.getState());
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,7 +1,9 @@
+import { createAction } from "@reduxjs/toolkit";
+
 // Action types
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
+// const BUG_ADDED = "bugAdded";
+// const BUG_REMOVED = "bugRemoved";
+// const BUG_RESOLVED = "bugResolved";
 
 //Action creators
 
@@ -14,25 +16,22 @@ const BUG_RESOLVED = "bugResolved";
 //   };
 // }
 
-export const bugAdded = (description) => ({
-  type: BUG_ADDED,
-  payload: {
-    description,
-  },
-});
+// export const bugAdded = (description) => ({
+//   type: BUG_ADDED,
+//   payload: {
+//     description,
+//   },
+// });
 
-export const bugResolved = (id) => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id,
-  },
-});
+export const bugAdded = createAction("bugAdded");
+export const bugResolved = createAction("bugResolved");
+export const bugRemoved = createAction("bugRemoved");
 
 // Reducer
 let lastId = 0;
 
 export default function reducer(state = [], action) {
-  if (action.type === BUG_ADDED)
+  if (action.type === bugAdded.type)
     return [
       ...state,
       {
@@ -41,9 +40,9 @@ export default function reducer(state = [], action) {
         resolved: false,
       },
     ];
-  else if (action.type === BUG_REMOVED)
+  else if (action.type === bugRemoved.type)
     return state.filter((bug) => bug.id !== action.payload.id);
-  else if (action.type === BUG_RESOLVED)
+  else if (action.type === bugResolved.type)
     return state.map((bug) =>
       bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
     );
