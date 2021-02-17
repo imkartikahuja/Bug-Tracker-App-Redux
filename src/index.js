88f4e0c41a2e6c350c37f95043da80c461d2b7ab
Kartik Ahuja <kartik.ahuja24@gmail.com>
2021-02-17 23:43:20 +0530

    Combining Reducers of bugs and projects

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
+store.dispatch(bugAdded({ description: "Bug 1" }));
+store.dispatch(bugAdded({ description: "Bug 2" }));
+store.dispatch(bugAdded({ description: "Bug 3" }));
 
-// store.dispatch(bugResolved({ id: 1 }));
+store.dispatch(bugResolved({ id: 1 }));
 
 store.dispatch(projectAdded({ name: "Project 1" }));
 
