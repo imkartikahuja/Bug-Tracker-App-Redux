4bcae60a153b1bd9ff92d1a84560e3236955f9bf
Kartik Ahuja <kartik.ahuja24@gmail.com>
2021-03-06 00:21:20 +0530

    cleaning index.js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,9 @@
 import configureStore from "./store/configureStore";
-import { addBug, loadBugs, resolveBug, assignBugToUser } from "./store/bugs";
-// import {
-//   bugAdded,
-//   bugResolved,
-//   bugAssignedToUser,
-//   getUnresolvedBugs,
-//   getBugsByUser,
-// } from "./store/bugs";
-// import { projectAdded } from "./store/projects";
-// import { userAdded } from "./store/users";
+import { loadBugs, resolveBug, assignBugToUser } from "./store/bugs";
 
 const store = configureStore();
-
-//CALLING API
-// store.dispatch(addBug({ description: "a" }));
 
 store.dispatch(loadBugs());
 
 setTimeout(() => store.dispatch(assignBugToUser(1, 4)), 2000);
 setTimeout(() => store.dispatch(resolveBug(1)), 2000);
-
-// store.dispatch((dispatch, getState) => {
-//   // Call an API
-//   // When the promise is resolved => dispatch()
-//   dispatch({ type: "bugsRecevied", bugs: [1, 2, 3] });
-//   console.log(getState());
-//   // If the promise is rejected => dispatch()
-// });
-
-// store.dispatch({
-//   type: "error",
-//   payload: { message: "An error occurred." },
-// });
-
-// store.dispatch(userAdded({ name: "Kartik Ahuja" }));
-// store.dispatch(userAdded({ name: "User 2" }));
-
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-// store.dispatch(bugResolved({ id: 1 }));
-
-// store.dispatch(projectAdded({ name: "Project 1" }));
-
-// const unresolvedBugs = getUnresolvedBugs(store.getState());
-// console.log(unresolvedBugs);
-
-// console.log(store.getState());
-
-// const bugs = getBugsByUser(1)(store.getState());
-// console.log(bugs);
